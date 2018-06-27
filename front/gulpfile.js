f496c3bf2c9637aa8ce6478ca7b2886ba7662fc3
freddy <freddy@MacBook-Pro-de-freddy.local>
2018-06-27 22:30:36 +0200

    bootstrap

---
+++ front/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function(){
+  return gulp.src('styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('../web/css'))
+});
+
+gulp.task('js', function(){
+  return gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js'])
+    .pipe(gulp.dest('../web/js'))
+});
+
+gulp.task('fonts', function(){
+  return gulp.src('bower_components/bootstrap-sass/assets/fonts/*/**')
+    .pipe(gulp.dest('../web/fonts'))
+});
+
+
+
+gulp.task('default', ['styles', 'js', 'fonts']);
